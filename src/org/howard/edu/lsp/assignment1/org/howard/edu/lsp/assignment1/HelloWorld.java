81cfc759aeaf56d138287d1e948bf927000ae896
jaila-bison <168598532+jaila-bison@users.noreply.github.com>
2026-08-25 11:12:22 -0400

    Recreate HelloWorld.java and moved README.md files

--- src/org/howard/edu/lsp/assignment1/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,6 +1,13 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Large Scale Programming - Assignment 1
+ * 
+ * @author Jaila
+ */
+
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!!");
     }
 }
-// Jaila Walker
